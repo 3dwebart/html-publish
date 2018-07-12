10fb7c64d7164a422fd4265a7366ce2743f46458
unknown <3d.web.artist@gmail.com>
2018-07-12 18:47:38 +0900

    2018-07-12 17:47

--- 00_MOBILE/js/MobileMainNavSubNav.js
+++ 00_MOBILE/js/MobileMainNavSubNav.js
@@ -1,20 +1,21 @@
 (function($) {
   $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open');
-    $('.offcanvas-collapse-bg').toggleClass('open');
-    $('.m-nav-close').addClass('open');
+    $('#panel-list').addClass('open');
   });
-  $('.offcanvas-collapse-bg').on('click', function () {
-    $('.offcanvas-collapse').removeClass('open');
-    $('.offcanvas-collapse-bg').removeClass('open');
-    $('.m-nav-close').removeClass('open');
+  $('.close').on('click', function () {
+    $('#panel-list').removeClass('open');
   });
-  $('.m-nav-close').on('click', function () {
-    $('.offcanvas-collapse').removeClass('open');
-    $('.offcanvas-collapse-bg').removeClass('open');
-    $('.m-nav-close').removeClass('open');
+  var x = 0;
+  $('#show-hide-info-btn').on('click', function () {
+    x = (x - 1) * -1;
+    if(x == 0) {
+      $(this).find('.show-hide').text('show');
+      $(this).find('.fas').attr('class', 'fas fa-angle-down');
+    } else {
+      $(this).find('.show-hide').text('hide');
+      $(this).find('.fas').attr('class', 'fas fa-angle-up');
+    }
   });
-
   $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
   });

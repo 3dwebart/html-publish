096fc3b1a4c4cf6ccb18ef05828d6fd9a5b14378
unknown <3d.web.artist@gmail.com>
2018-07-24 14:49:55 +0900

    2018-07-24 14:50 files : 00_MOBILE/balances.html | 00_MOBILE/guide.html | 00_MOBILE/js/MobileMainNavSubNav.js | 00_MOBILE/m_faq-tabs.html | 00_MOBILE/m_faq.html | 00_MOBILE/m_operation_guide.html | 00_MOBILE/otp.html | 00_MOBILE/presence.html | 00_MOBILE/privacy.html | 00_MOBILE/question_write.html | 00_MOBILE/questionlist.html | 00_MOBILE/return_Email_Confirm.html | 00_MOBILE/return_Email_ConfirmPWD.html | 00_MOBILE/return_Email_Confirm_Withdraw.html | 00_MOBILE/signedit.html | 00_MOBILE/signin_otp.html | 00_MOBILE/signrequest.html | 00_MOBILE/signrequestpwd.html | 00_MOBILE/signup.html | 00_MOBILE/trade-backup.html | 00_MOBILE/trade-coin-list.html | 00_MOBILE/trade.html | 00_MOBILE/trade_order.html | 00_MOBILE/trade_quotes.html | 00_MOBILE/verificationcenter.html | 00_MOBILE/verificationform.html | 00_MOBILE/verificationformmobile.html

--- 00_MOBILE/js/MobileMainNavSubNav.js
+++ 00_MOBILE/js/MobileMainNavSubNav.js
@@ -1,26 +1,22 @@
 (function($) {
-  $('[data-toggle="offcanvas"]').on('click', function () {
-    $('#panel-list').addClass('open');
-  });
-  $('.close').on('click', function () {
-    $('#panel-list').removeClass('open');
-  });
-  var x = 0;
-  $('#show-hide-info-btn').on('click', function () {
-    x = (x - 1) * -1;
-    if(x == 0) {
-      $(this).find('.show-hide').text('show');
-      $(this).find('.fas').attr('class', 'fas fa-angle-down');
-    } else {
-      $(this).find('.show-hide').text('hide');
-      $(this).find('.fas').attr('class', 'fas fa-angle-up');
-    }
-  });
-  $('.dropdown').on('show.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
-  });
+	/* BIGIN :: 모바일 메뉴 클릭시 좌에서 우로 슬라이드 되는 메뉴 */
+	// 메뉴 open
+	$('[data-toggle="offcanvas"]').on('click', function () {
+		$('#panel-list').addClass('open');
+	});
+	// 메뉴 close
+	$('.close').on('click', function () {
+		$('#panel-list').removeClass('open');
+	});
+	/* BIGIN :: 모바일 메뉴 클릭시 좌에서 우로 슬라이드 되는 메뉴 */
 
-  $('.dropdown').on('hide.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
-  });
+	/* BIGIN :: 모바일 메뉴 오픈시 하단 언어 선택부분 애니메이션 스크립트 */
+	$('.dropdown').on('show.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+	});
+
+	$('.dropdown').on('hide.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+	});
+	/* END :: 모바일 메뉴 오픈시 하단 언어 선택부분 애니메이션 스크립트 */
 })(jQuery);
